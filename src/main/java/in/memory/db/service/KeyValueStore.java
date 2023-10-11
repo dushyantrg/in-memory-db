4e0bc7f111edeec0149c1b232f1ed5723d5ea9a9
dushyantrg <dushyang@sahaj.ai>
2023-10-11 15:05:20 +0530

    Eagerly loading KeyValueStore so that log file can be processed before the first api request

--- src/main/java/in/memory/db/service/KeyValueStore.java
+++ src/main/java/in/memory/db/service/KeyValueStore.java
@@ -1,12 +1,12 @@
 package in.memory.db.service;
 
+import io.micronaut.context.annotation.Context;
 import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
 
 import java.io.IOException;
 import java.util.Map;
 
-@Singleton
+@Context
 public class KeyValueStore {
 
     private final AppendOnlyLog appendOnlyLog;
