37d2a58ea979ac3a1724b9149c24cf2e8bf1c77d
dushyantrg <dushyang@sahaj.ai>
2023-10-11 20:49:16 +0530

    Returning Reactive types from Controller

--- src/main/java/in/memory/db/controller/DataController.java
+++ src/main/java/in/memory/db/controller/DataController.java
@@ -3,6 +3,7 @@ package in.memory.db.controller;
 import in.memory.db.service.KeyValueStore;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -18,12 +19,13 @@ public class DataController {
 
     @Get("/{key}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getValueForKey(@PathVariable(name= "key") String key)  {
-        return this.keyValueStore.getValue(key);
+    public Mono<String> getValueForKey(@PathVariable(name= "key") String key)  {
+        return Mono.just(this.keyValueStore.getValue(key));
     }
 
     @Put("/{key}/value/{value}")
-    public void updateValueForKey(@PathVariable(name= "key") String key, @PathVariable(name= "value") String value) throws IOException {
+    public Mono<Void> updateValueForKey(@PathVariable(name= "key") String key, @PathVariable(name= "value") String value) throws IOException {
         this.keyValueStore.putValue(key, value);
+        return Mono.empty();
     }
 }
