b958561715544bad01000b33c988d7d20c235989
dushyantrg <dushyang@sahaj.ai>
2023-10-11 14:09:04 +0530

    Moving printExecutionTimes to HashMapSpeedEstimate

--- src/main/java/in/memory/db/Application.java
+++ src/main/java/in/memory/db/Application.java
@@ -10,31 +10,4 @@ public class Application {
 
         Micronaut.run(Application.class, args);
     }
-
-    private static void printExecutionTimes() throws IOException {
-        HashMapSpeedEstimate estimate = new HashMapSpeedEstimate();
-        String baseString = "key0123456789012345678901234567890123456789012345678901234567890";
-        long count = 10000000;
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-        estimate.printHashMapInsertTimes(baseString, count);
-
-
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-        estimate.printFileAppendTimes(baseString, (int)count);
-    }
 }
--- src/main/java/in/memory/db/HashMapSpeedEstimate.java
+++ src/main/java/in/memory/db/HashMapSpeedEstimate.java
@@ -103,4 +103,31 @@ public class HashMapSpeedEstimate {
         }
         return list;
     }
+
+    private static void printExecutionTimes() throws IOException {
+        HashMapSpeedEstimate estimate = new HashMapSpeedEstimate();
+        String baseString = "key0123456789012345678901234567890123456789012345678901234567890";
+        long count = 10000000;
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+        estimate.printHashMapInsertTimes(baseString, count);
+
+
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+        estimate.printFileAppendTimes(baseString, (int)count);
+    }
 }
